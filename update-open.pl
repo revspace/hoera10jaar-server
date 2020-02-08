80288197b023278dec893533925341d8f061e59e
Hoera10jaar <hoera10jaar@revspace.nl>
2020-02-08 17:44:02 +0100

    Achterstallige commits

--- update-open.pl
+++ update-open.pl
@@ -16,28 +16,28 @@ sub slurp { local (@ARGV, $/) = shift; scalar <> }
 sub spurt { open my $fh, ">", shift; print $fh @_; close $fh; }
 
 my $urls = $json->decode(slurp "urls.json");
-my %open;
+my %all;
 
 for my $k (keys %$urls) {
-    $open{$k} = undef;
+    my $open;
     eval {
         if ($k eq 'utrecht') {  # speciaaltje
             # Van https://randomdata.nl/assets/js/spacestate.js
-            $open{$k} = $json->decode(get(
+            $open = $json->decode(get(
                 "http://api.thingspeak.com/channels/886012/feed/last.json?api_key=A0VXRM8FPPKUT031"
             ))->{field1} ? Types::Serialiser::true : Types::Serialiser::false;
-            next;
+        } else {
+            my $url = $urls->{$k} or next;
+            my $hash = $json->decode(get $url);
+
+            # if old api and new api disagree in one state file, state is unknown
+            $open = $hash->{open} // $hash->{state}->{open};
+
+            $open = undef if defined $hash->{open} and defined $hash->{state}->{open}
+                          and        $hash->{open}    !=       $hash->{state}->{open};
+
+            $all{$k} = $open;
         }
-
-        my $url = $urls->{$k} or next;
-	my $hash = $json->decode(get $url);
-
-        # if old api and new api disagree in one state file, state is unknown
-        my $open = $hash->{open} // $hash->{state}->{open};
-        $open = undef if defined $hash->{open} and defined $hash->{state}->{open}
-                         and     $hash->{open}  !=         $hash->{state}->{open};
-
-        $open{$k} = $open;
 
         $mqtt->publish("hoera10jaar/spaceapi/$k",
             defined $open
@@ -45,7 +45,10 @@ for my $k (keys %$urls) {
             : 'yellow'
         );
     };
-    warn "$k: $@" if $@;
+    if ($@) {
+        warn "$k: $@";
+        $mqtt->publish("hoera10jaar/spaceapi/$k", 'yellow');
+    }
 }
 
-spurt "open.json", $json->encode(\%open);
+spurt "all.json", $json->encode(\%all);
